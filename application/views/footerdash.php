212bfd8dc564095a4b2d79fd0f6bd7e73ca4dd0d
Sergio Moyano <sergio.moyano@outlook.com.ar>
2015-09-02 20:50:58 -0300

    modificar referencia a sitio

--- application/views/footerdash.php
+++ application/views/footerdash.php
@@ -2,7 +2,7 @@
         <div class="pull-right hidden-xs">
           <b>Version</b> 1.0.0
         </div>
-        <strong>Copyright &copy; 2015-2016 <a href="http://almsaeedstudio.com">Indev</a>.</strong> Todos los derechos reservados.
+        <strong>Copyright &copy; 2015-2016 <a href="http://indevla.com">Indev</a>.</strong> Todos los derechos reservados.
       </footer>
 
       
